e03f71e7e155c77731448b476103522544919062
kuldeepthedev <kuldeep.kumar@deorwine.com>
2024-05-15 16:33:56 +0530

    update theme

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,12 +1,15 @@
 "use client";
 import DashboardLayout from "./layout/dashboard";
 import { QueryClient, QueryClientProvider } from "react-query";
+import ThemeProvider from "./theme";
 
 export default function RootLayout({ children }) {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <DashboardLayout>{children}</DashboardLayout>
+      <ThemeProvider>
+        <DashboardLayout>{children}</DashboardLayout>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
